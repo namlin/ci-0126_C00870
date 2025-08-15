f310840527c848a2b69ab2a06634b5cdea61ebb0
N.Lin <70311238+namlin@users.noreply.github.com>
2025-08-14 18:32:07 -0600

    Base files generated by default for the project.

---
+++ Laboratorio1_C00870/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_C00870.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
